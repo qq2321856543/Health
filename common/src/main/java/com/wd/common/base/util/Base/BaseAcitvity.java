3765f08476be148d63b0b19364b6f4e6f86fa197
Administrator <2321856543@qq.com>
2020-05-19 12:32:29 +0800

    base

---
+++ common/src/main/java/com/wd/common/base/util/Base/BaseAcitvity.java
@@ -0,0 +1,74 @@
+package com.wd.common.base.util.Base;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
+import com.jaeger.library.StatusBarUtil;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseAcitvity<P extends BasePresenter> extends AppCompatActivity implements IBaseView {
+
+    private P mPresenter;
+    private Dialog mLoadingDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(getLayout());
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().hide();
+        }
+        StatusBarUtil.setTransparent(this);
+        ButterKnife.bind(this);
+        mPresenter = initPresenter();
+
+        initView();
+        initData();
+    }
+    public P getPresenter(){
+        return mPresenter;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPresenter!=null){
+            mPresenter.detachView();
+            mPresenter=null;
+        }
+    }
+//    public void showDialog(){
+//        if (mLoadingDialog==null){
+//            mLoadingDialog = new Dialog(this);
+//            mLoadingDialog.setCancelable(false);
+//            View view = View.inflate(this, R.layout.dialog_loading, null);
+//            ImageView iv=view.findViewById(R.id.iv);
+//            Glide.with(this).asGif().load(R.mipmap.timg).into(iv);
+//            mLoadingDialog.addContentView(view,
+//                    new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+//                            ViewGroup.LayoutParams.WRAP_CONTENT));
+//        }
+//        mLoadingDialog.show();
+//    }
+    public void hideDialog() {
+        if (mLoadingDialog != null && mLoadingDialog.isShowing()) {
+            mLoadingDialog.dismiss();
+        }
+    }
+    protected abstract P initPresenter();
+
+    protected abstract int getLayout();
+
+    protected abstract void initView();
+
+    protected abstract void initData();
+}
---
+++ common/src/main/java/com/wd/common/base/util/Base/BaseFragment.java
@@ -0,0 +1,95 @@
+package com.wd.common.base.util.Base;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
+import com.jaeger.library.StatusBarUtil;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseFragment<P extends BasePresenter> extends Fragment {
+
+    private P presenter;
+    Boolean is=false;
+    private Dialog mLoadingDialog;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(getActivity(), getLayout(), null);
+        presenter = initPresenter();
+        ButterKnife.bind(this,view);
+        StatusBarUtil.setTransparent(getActivity());
+        initView(view);
+        Log.i("ppp","onCreateView");
+        is=true;
+        doNetWork();
+        return view;
+    }
+    private void doNetWork(){
+        if (getUserVisibleHint()){
+            initData();
+            Log.i("ppp","doNetWork"+is);
+
+        }
+    }
+
+    protected abstract P initPresenter();
+
+    protected abstract int getLayout();
+
+    protected abstract void initView(View view);
+
+
+    protected abstract void initData();
+
+    public P getPresenter(){
+        return presenter;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        Log.i("ppp","setUserVisibleHint");
+
+        if (is&&isVisibleToUser){
+            initData();
+            Log.i("ppp","setUserVisibleHint"+is+isVisibleToUser);
+
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i("ppp","onDetach");
+
+    }
+//    public void showDialog(){
+//        if (mLoadingDialog==null){
+//            mLoadingDialog = new Dialog(getContext());
+//            mLoadingDialog.setCancelable(false);
+//            View view = View.inflate(getContext(), R.layout.dialog_loading, null);
+//            ImageView iv=view.findViewById(R.id.iv);
+//            Glide.with(this).asGif().load(R.mipmap.timg).into(iv);
+//            mLoadingDialog.addContentView(view,
+//                    new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+//                            ViewGroup.LayoutParams.WRAP_CONTENT));
+//        }
+//        mLoadingDialog.show();
+//    }
+    public void hideDialog() {
+        if (mLoadingDialog != null && mLoadingDialog.isShowing()) {
+            mLoadingDialog.dismiss();
+        }
+    }
+}
