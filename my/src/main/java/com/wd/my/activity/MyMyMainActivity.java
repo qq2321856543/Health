8eea4896521cdfb4bed81b87f912a46a7c230c33
Administrator <2321856543@qq.com>
2020-05-19 20:54:46 +0800

    我的页面UI5%

---
+++ my/src/main/java/com/wd/my/activity/MyMyMainActivity.java
@@ -0,0 +1,32 @@
+package com.wd.my.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wd.common.base.util.Base.BaseAcitvity;
+import com.wd.common.base.util.Base.BasePresenter;
+import com.wd.my.R;
+
+public class MyMyMainActivity extends BaseAcitvity {
+
+
+    @Override
+    protected BasePresenter initPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_my_my_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
