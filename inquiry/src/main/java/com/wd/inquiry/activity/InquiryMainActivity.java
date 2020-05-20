1e6088ee0c71cfba5c03dc94b3b7d091c55af1ae
Administrator <2321856543@qq.com>
2020-05-20 20:59:56 +0800

    极光IM配置完成

---
+++ inquiry/src/main/java/com/wd/inquiry/activity/InquiryMainActivity.java
@@ -0,0 +1,34 @@
+package com.wd.inquiry.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wd.common.base.util.Base.BaseAcitvity;
+import com.wd.common.base.util.Base.BasePresenter;
+import com.wd.inquiry.R;
+
+import cn.jpush.im.android.api.JMessageClient;
+
+public class InquiryMainActivity extends BaseAcitvity {
+
+
+    @Override
+    protected BasePresenter initPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_inquiry_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ inquiry/src/main/java/com/wd/inquiry/service/XService.java
@@ -0,0 +1,8 @@
+package com.wd.inquiry.service;
+
+
+import cn.jpush.android.service.JCommonService;
+
+public class XService extends JCommonService {
+
+}
