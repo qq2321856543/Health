2eaacfd316e877c8c5f4445c1067bfe0691e6c2d
qq767986297 <767986297@qq.com>
2020-05-20 16:22:31 +0800

    王冠华

--- app/src/main/java/com/wd/health/App.java
+++ app/src/main/java/com/wd/health/App.java
@@ -1,6 +1,13 @@
 package com.wd.health;
 
+import android.support.multidex.MultiDex;
+
 import com.wd.common.base.util.Base.BaseApplication;
 
 public class App extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MultiDex.install(this);
+    }
 }
