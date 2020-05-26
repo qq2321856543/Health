3c3cc9bfff93d938b65d7db18dfe4b74813c3571
qq767986297 <767986297@qq.com>
2020-05-26 14:02:51 +0800

    王冠华5.26

---
+++ home/src/main/java/com/wd/home/activity/HomeKnowledgeActivity.java
@@ -0,0 +1,32 @@
+package com.wd.home.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wd.common.base.util.Base.BaseAcitvity;
+import com.wd.common.base.util.Base.BasePresenter;
+import com.wd.home.R;
+
+public class HomeKnowledgeActivity extends BaseAcitvity {
+
+
+    @Override
+    protected BasePresenter initPresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_home_knowledge;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ home/src/main/java/com/wd/home/bean/HomeDiseaseDetailBean.java
@@ -0,0 +1,119 @@
+package com.wd.home.bean;
+
+/**
+ * Time: 2020/5/26
+ * Author: 王冠华
+ * Description:
+ */
+public class HomeDiseaseDetailBean {
+
+    /**
+     * result : {"benefitTaboo":" 1．滑囊炎不是严重的疾病，通常它会在1~2周内自动痊愈，所以不用着急。 2．加强劳动保护，养成劳作后用温水洗手的习惯。 3．经常按摩关节附近的穴位和肌肉。","chineseMedicineTreatment":" 1．可用小活络片和消炎痛。 2．中药洗方薰洗如桑枝、桂枝、槐枝、柳枝、榆枝各60克，水煎外洗。 3．外敷三色敷药。","createTime":1547108512000,"diseaseCategoryId":18,"id":18,"pathology":"在关节附近的皮肤与皮下骨骼之间，或在腱与骨骼间有一种充满润滑液的软囊，可使身体组织相互间在活动时的磨擦减到最小。如果滑囊受到过重的压力，或是受到关节或关节附近组织损伤的刺激，就会发炎，形成滑囊炎。肘、拇趾底、髋、跟及肩等关节容易患滑囊炎。","symptom":"滑囊炎是一种常见的疾病，会造成囊四周肿胀、疼痛。"}
+     * message : 查询成功
+     * status : 0000
+     */
+
+    private ResultBean result;
+    private String message;
+    private String status;
+
+    public ResultBean getResult() {
+        return result;
+    }
+
+    public void setResult(ResultBean result) {
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public static class ResultBean {
+        /**
+         * benefitTaboo :  1．滑囊炎不是严重的疾病，通常它会在1~2周内自动痊愈，所以不用着急。 2．加强劳动保护，养成劳作后用温水洗手的习惯。 3．经常按摩关节附近的穴位和肌肉。
+         * chineseMedicineTreatment :  1．可用小活络片和消炎痛。 2．中药洗方薰洗如桑枝、桂枝、槐枝、柳枝、榆枝各60克，水煎外洗。 3．外敷三色敷药。
+         * createTime : 1547108512000
+         * diseaseCategoryId : 18
+         * id : 18
+         * pathology : 在关节附近的皮肤与皮下骨骼之间，或在腱与骨骼间有一种充满润滑液的软囊，可使身体组织相互间在活动时的磨擦减到最小。如果滑囊受到过重的压力，或是受到关节或关节附近组织损伤的刺激，就会发炎，形成滑囊炎。肘、拇趾底、髋、跟及肩等关节容易患滑囊炎。
+         * symptom : 滑囊炎是一种常见的疾病，会造成囊四周肿胀、疼痛。
+         */
+
+        private String benefitTaboo;
+        private String chineseMedicineTreatment;
+        private long createTime;
+        private int diseaseCategoryId;
+        private int id;
+        private String pathology;
+        private String symptom;
+
+        public String getBenefitTaboo() {
+            return benefitTaboo;
+        }
+
+        public void setBenefitTaboo(String benefitTaboo) {
+            this.benefitTaboo = benefitTaboo;
+        }
+
+        public String getChineseMedicineTreatment() {
+            return chineseMedicineTreatment;
+        }
+
+        public void setChineseMedicineTreatment(String chineseMedicineTreatment) {
+            this.chineseMedicineTreatment = chineseMedicineTreatment;
+        }
+
+        public long getCreateTime() {
+            return createTime;
+        }
+
+        public void setCreateTime(long createTime) {
+            this.createTime = createTime;
+        }
+
+        public int getDiseaseCategoryId() {
+            return diseaseCategoryId;
+        }
+
+        public void setDiseaseCategoryId(int diseaseCategoryId) {
+            this.diseaseCategoryId = diseaseCategoryId;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getPathology() {
+            return pathology;
+        }
+
+        public void setPathology(String pathology) {
+            this.pathology = pathology;
+        }
+
+        public String getSymptom() {
+            return symptom;
+        }
+
+        public void setSymptom(String symptom) {
+            this.symptom = symptom;
+        }
+    }
+}
