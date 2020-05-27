43d89eaaeb324294b28a9234233c64c1bb795353
Administrator <2321856543@qq.com>
2020-05-27 09:23:30 +0800

    Merge branch 'cong'
    # Conflicts:
    #	common/src/main/java/com/wd/common/base/util/Base/BaseApplication.java
    #	common/src/main/java/com/wd/common/base/util/util/RetrofitUtil.java
    #	inquiry/build.gradle
    #	inquiry/src/main/AndroidManifest.xml
    #	inquiry/src/main/java/com/wd/inquiry/activity/InquiryMainActivity.java
    #	inquiry/src/main/java/com/wd/inquiry/base/InquiryApis.java
    #	inquiry/src/main/java/com/wd/inquiry/bean/DoctorListBean.java
    #	inquiry/src/main/java/com/wd/inquiry/fragment/Fragment_Comprehensive.java
    #	inquiry/src/main/res/layout/activity_inquiry_main.xml
    #	inquiry/src/main/res/layout/fragment_comprehensive.xml
    #	login/src/main/AndroidManifest.xml
    #	login/src/main/java/com/wd/login/activity/LoginLonginActivity.java
    #	login/src/main/java/com/wd/login/activity/activity/LoginForgetPassWordActivity.java
    #	login/src/main/java/com/wd/login/activity/activity/LoginLonginActivity.java
    #	login/src/main/java/com/wd/login/activity/activity/LoginRegisterActivity.java
    #	login/src/main/java/com/wd/login/activity/activity/LoginSetNewPassWordActivity.java
    #	login/src/main/java/com/wd/login/activity/util/LoginApis.java
    #	login/src/main/res/layout/activity_login_forget_pass_word.xml
    #	login/src/main/res/layout/activity_login_longin.xml
    #	login/src/main/res/layout/activity_login_register.xml
    #	login/src/main/res/layout/activity_login_set_new_pass_word.xml
    #	my/src/main/AndroidManifest.xml
    #	my/src/main/java/com/wd/my/activity/MyMyMainActivity.java
    #	my/src/main/res/layout/activity_my_my_main.xml
    #	my/src/main/res/mipmap/my_background.png
    #	patient/build.gradle

--- login/src/main/java/com/wd/login/activity/activity/LoginLonginActivity.java
+++ login/src/main/java/com/wd/login/activity/activity/LoginLonginActivity.java
@@ -170,13 +170,15 @@ public class LoginLonginActivity extends BaseAcitvity implements ILoginContract.
              }
             Log.i("xxx","极光IM帐号，帐号："+result.getUserName()+";密码："+result.getJiGuangPwd()+";解密后的密码:"+s+";MD5加密后的密码:"+MD.MD5(s));
             //极光IM密码MD5加密 并登录
-            JMessageClient.login(result.getUserName(), "123456", new BasicCallback() {
+            JMessageClient.login(result.getUserName(), MD.MD5(s), new BasicCallback() {
                 @Override
                 public void gotResult(int i, String s) {
                     if (i==0){
                         Log.i("xxx","极光IM登录成功");
+                        Toast.makeText(LoginLonginActivity.this, "极光IM登录成功", Toast.LENGTH_SHORT).show();
                     }else {
                         Log.i("xxx","极光IM登录失败："+i+"///"+s);
+                        Toast.makeText(LoginLonginActivity.this, "极光IM登录失败", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
